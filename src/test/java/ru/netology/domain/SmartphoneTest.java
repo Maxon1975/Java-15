af3ed5ff16594acc7d247b89668d2dc316380913
Maxim Annenkov <bakeeva@yandex.ru>
2022-01-23 18:15:48 +0300

    initial fixation

---
+++ src/test/java/ru/netology/domain/SmartphoneTest.java
@@ -0,0 +1,25 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.manager.ProductManager;
+import ru.netology.repository.ProductRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SmartphoneTest {
+    @Test
+    public void shouldHaveAllMethodsFromSuper() {
+        Smartphone smartphone = new Smartphone();
+
+    }
+    @Test
+    public void shouldEUseEquals() {
+        Smartphone smartphone1 =new Smartphone(2,"Sony",1000,"RRR");
+        Smartphone smartphone2 =new Smartphone(2,"Sony",1000,"RRR");
+
+        assertEquals(smartphone1,smartphone2);
+
+    }
+
+
+}
